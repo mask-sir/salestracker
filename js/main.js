0e54fec5cf02c11ac0c723c9f2a43aa3a8924ad6
MASK SER <94510246+mask-sir@users.noreply.github.com>
2025-09-14 10:39:40 +0530

    Set up Firebase configuration and initialization

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+// --- FIREBASE SETUP ---
+const firebaseConfig = {
+  apiKey: "AIzaSyCAjL7T72-kq6DL7gWzzfpqswcNTqdkjZ0",
+  authDomain: "sales-pro-83fc2.firebaseapp.com",
+  databaseURL: "https://sales-pro-83fc2-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sales-pro-83fc2",
+  storageBucket: "sales-pro-83fc2.firebasestorage.app",
+  messagingSenderId: "727475911328",
+  appId: "1:727475911328:web:b484d326d578d58819a7c5",
+  measurementId: "G-MRK7K77X9T"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Firestore reference
+const db = firebase.firestore();
 document.addEventListener('DOMContentLoaded', () => {
 
     // --- App State ---
